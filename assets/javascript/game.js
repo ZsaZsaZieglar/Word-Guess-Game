f73249136002bc3376673d0642d0f92ed8d49c63
ZsaZsaZieglar <zsazsazigler@gmail.com>
2018-09-11 13:05:04 -0400

    infinite loop,comment out WHILE loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,71 +1,72 @@
 // Word base for user to guess 
+
 var word = [
     "rottweiler",
     "mastiff",
     "poodle",
     "collie"
-   ];  
-  
+   ]; 
+   //store randomly picked word 
+   var pickedLetter;
+   //store picked word's characters
+   var pickedLetterArray = [];
+   //placeholders for characters
+   var pickedLetterPlaceholder = [];
+   var win =0;
+   var loss = 0;
+   //store the guessed characters
+   var lettersGuessed = [];
+   var guessesLeft;
+
+   function play()
+   { 
+       guessesLeft = 15;
+       document.getElementById("guessLeft").innerHTML = guessesLeft;
+       lettersGuessed = [];
+
   // Computer selects a random word from the word base and substitue blanks 
-  var word = word[Math.floor(Math.random()*word.length)];
-  var answerWord = [];
-  for (var i = 0; i < word.length; i++){
-      answerWord[i] = "_";
+  var pickedLetter = word[Math.floor(Math.random()*word.length)];
+  var pickedLetterArray = pickedLetter.split(""); //["p","o","o","d","l","e"]
+
+  pickedLetterPlaceholder = [];
+  for (var i = 0; i < pickedLetterArray.length; i++){
+      pickedLetterPlaceholder.push("___ ");
   }
-  console.log(answerWord);
-
-  // Checks length of the word and stores it in a variable to see if length is less than zero
-  var lettersRemain = word.length;
-  while (lettersRemain > 0){
-      alert(answerWord.join(" "));
-      
-  // Prompts user to press a letter key      
-  var playerGuess = prompt("Press any letter key to get started");
-
-  // Checks for one letter input by user
-      function oneLetter()
-          {
-            var charCode = event.KeyCode;
-            if(!(charCode > 64) && (!(charCode < 91)))
-               {
-                   alert("Please enter a single letter");
-               }             
-            else
-               {
-                   //Continues to update the game
-                   for (var j = 0; j < word.length; j++)
-                    {
-                       if  (word[j] === playerGuess)
-                       {
-                           answerWord[j] = playerGuess;
-                           lettersRemain--;
-                       }
-                   }
-               }
-                    
-        }
-    }
-  /*
-  // Function call to check input of user
-  oneLetter();  
-
-  // Displays the answer
-  console.log(answerWord.join(" "));
-  console.log("The answer was " + word);
-  
- //  Update the DOM for WINS, GUESS REMAINING, and LETTERS GUESSED
- function winFunction() 
-   {
-      document.getElementById("wins").innerHTML = word;   
+  var placeholderString = pickedLetterPlaceholder.join("");
+  document.getElementById("placeholders").innerHTML = placeholderString;
    }
 
- function guessRemain()
-   {
-      document.getElementById("guesses").innerHTML = lettersRemain; 
-   } 
-   
- function alreadyGuessed()
-   {
-      document.getElementById("lettersGuessed").innerHTML = answerWord.join(" ");
-   }   
-  */
+   document.onkeyup = function(event){
+       var userGuess = event.key;
+        for(var i=0;i<pickedLetterArray.length;i++){
+            //guessed letter check
+            if(userGuess === pickedLetterArray[i]){
+                pickedLetterPlaceholder[i] = userGuess;
+                document.getElementById("placeholders").innerHTML = pickedLetterPlaceholder;
+            }
+        }
+
+        if(lettersGuessed.indexOf(userGuess)!= -1){
+            document.getElementById("err_msg").innerHTML = ("Please select another Character");
+        }
+        else{
+            guessesLeft--;
+
+            if(guessesLeft === 0)
+            {
+                loss++;
+                document.getElementById('losses').innerHTML = loss;
+                play();
+            }
+
+            lettersGuessed.push(userGuess);
+            document.getElementById("err_msg").innerHTML = "";
+            document.getElementById("guessed_char").innerHTML = lettersGuessed;
+            document.getElementById("guessLeft").innerHTML = guessesLeft;
+        }
+
+        if(pickedLetterArray.join("") === pickedLetter){
+            win++;
+            document.getElementById("wins").innerHTML = win;
+        }
+   }
